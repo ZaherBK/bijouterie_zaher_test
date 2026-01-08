b29d4b3c7a6d32a661b6b5b0baf319b13fe3bb7b
ZaherBK <zaherbijouterie@gmail.com>
2026-01-08 01:36:09 +0100

    final sync before push

--- auto_sync.py
+++ auto_sync.py
@@ -23,7 +23,7 @@ import os
 
 # ===================== CONFIGURATION =====================
 CLOUD_API_URL = "https://hr-sync.onrender.com"  # Your hosted website URL
-CLOUD_EMAIL = "zaher@local"
+CLOUD_EMAIL = "zaher@local.com"
 CLOUD_PASSWORD = "5"
 
 LOCAL_DB_HOST = "localhost"
