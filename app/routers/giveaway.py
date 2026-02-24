35a250a6e0834ef39f5de9bc2f2f26a2bbb26405
ZaherBK <zaherbijouterie@gmail.com>
2026-02-24 15:40:56 +0100

    fix(giveaway): add mandatory pages_read_user_content scope for reading Facebook comments

--- app/routers/giveaway.py
+++ app/routers/giveaway.py
@@ -48,7 +48,7 @@ async def facebook_login(request: Request):
     redirect_uri = "https://hr-sync.onrender.com/giveaways/auth/callback"
     
     # We request permissions to read pages and manage comments
-    permissions = "pages_show_list,pages_read_engagement,pages_manage_metadata,business_management"
+    permissions = "pages_show_list,pages_read_engagement,pages_read_user_content,pages_manage_metadata,business_management"
     
     oauth_url = f"https://www.facebook.com/v19.0/dialog/oauth?" + urlencode({
         "client_id": app_id,
